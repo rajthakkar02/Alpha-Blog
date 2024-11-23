020714bb63d6a43e12009748e90f2dc209d4f5e5
rajthakkar02 <rajthakkar0302@gmail.com>
2024-11-23 15:47:16 +0530

    Create one to many association between user and article

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_23_053124) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_23_100317) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
