d8ce0d03a4687fc6e79268db195801e4ba70159e
rajthakkar02 <rajthakkar0302@gmail.com>
2024-12-03 20:44:05 +0530

    Restrict action from Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [ :show, :edit, :update ]
+  before_action :set_user, only: [ :show, :edit, :update, :destroy ]
+  before_action :require_user, only: [ :edit, :update ]
+  before_action :require_same_user, only: [ :edit, :update, :destroy ]
 
   def show
     @articles = @user.articles.paginate(page: params[:page], per_page: 1)
@@ -36,6 +38,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user.destroy
+    flash[:notice] = "Account and all associated articles successfully deleted"
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
   private
 
   def user_params
@@ -44,5 +53,12 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:alert] = "You can only edit or delete your own user"
+      redirect_to @user
+    end
   end
 end
