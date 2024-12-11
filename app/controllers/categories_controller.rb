453ffa03751040e0c66b27b022b0e1bbb0b6f2bf
rajthakkar02 <rajthakkar0302@gmail.com>
2024-12-11 15:46:33 +0530

    Add functional test and integration test in category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,39 @@
 class CategoriesController < ApplicationController
+  before_action :require_admin, except: [ :index, :show ]
+
   def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    @category.name.capitalize!
+    if @category.save
+      flash[:notice] = "Category was created successfully"
+      redirect_to @category
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def index
+    @categories = Category.paginate(page: params[:page], per_page: 5)
   end
 
   def show
+    @category = Category.find(params[:id])
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+  def require_admin
+    if !(logged_in? && current_user.admin?)
+      flash[:alert] = "Only admins can perform that action"
+      redirect_to categories_path
+    end
   end
 end
---
+++ test/integration/list_categories_test.rb
@@ -0,0 +1,14 @@
+require "test_helper"
+
+class ListCategoriesTest < ActionDispatch::IntegrationTest
+  def setup
+    @category = Category.create(name: "Travel")
+    @category1 = Category.create(name: "Sports")
+  end
+
+  test "should show categories listing" do
+    get "/categories"
+    assert_select "a[href=?]", categories_path(@category), text: @category.name
+    assert_select "a[href=?]", categories_path(@category1), text: @category1.name
+  end
+end
