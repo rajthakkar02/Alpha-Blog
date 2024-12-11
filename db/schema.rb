05280c702bc1725a0af3b42838dd7a0635d4e383
rajthakkar02 <rajthakkar0302@gmail.com>
2024-12-11 23:28:55 +0530

    Edit Category name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_06_032721) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_11_104512) do
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
