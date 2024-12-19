bb065e53cf1f7bf0e5a076fcd0990214302a3bee
rajthakkar02 <rajthakkar0302@gmail.com>
2024-12-19 09:25:35 +0530

    Validation update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [ :show, :edit, :update, :destroy ]
-  before_action :require_user, only: [ :edit, :update ]
-  before_action :require_same_user, only: [ :edit, :update, :destroy ]
-  before_action :require_for_signup, only: [ :new ]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_for_signup, only: [:new]
 
   def show
     @articles = @user.articles.paginate(page: params[:page], per_page: 1)
